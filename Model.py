4f692934566d72156ad70598103d3343eb4e2e08
JPOonGIT <94445942+JPOonGIT@users.noreply.github.com>
2022-06-27 19:20:54 +0200

    27.06

--- Model.py
+++ Model.py
@@ -9,29 +9,29 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         zwischenlayer = 100
-        self.bert = BertModel.BertModel.from_pretrained("bert-base-uncased")
+        self.bert = BertModel.from_pretrained("bert-base-uncased")
         self.linear11 = nn.Linear(768, zwischenlayer)
-        self.linear21 = nn.Linear(768, zwischenlayer)
-        self.linear31 = nn.Linear(768, zwischenlayer)
-        self.linear41 = nn.Linear(768, zwischenlayer)
+        #self.linear21 = nn.Linear(768, zwischenlayer)
+        #.linear31 = nn.Linear(768, zwischenlayer)
+        #self.linear41 = nn.Linear(768, zwischenlayer)
         self.linear12 = nn.Linear(zwischenlayer, 1)
-        self.linear22 = nn.Linear(zwischenlayer, 1)
-        self.linear32 = nn.Linear(zwischenlayer, 1)
-        self.linear42 = nn.Linear(zwischenlayer, 1)
+        #self.linear22 = nn.Linear(zwischenlayer, 1)
+        #self.linear32 = nn.Linear(zwischenlayer, 1)
+        #self.linear42 = nn.Linear(zwischenlayer, 1)
 
-    def forward(self, input_ids, attention_mask):
+    def forward(self, input_ids, attention_mask = None):
         bert_output = self.bert(input_ids, attention_mask=attention_mask)
-        bert_output = bert_output[0].view(-1,768)
+        bert_output = bert_output[0].view(-1,1)
         x1 = F.relu(self.linear11(bert_output))
         x1 = F.relu(self.linear12(x1))
-        x2 = F.relu(self.linear21(bert_output))
+        '''x2 = F.relu(self.linear21(bert_output))
         x2 = F.relu(self.linear22(x2))
         x3 = F.relu(self.linear31(bert_output))
         x3 = F.relu(self.linear32(x3))
         x4 = F.relu(self.linear41(bert_output))
-        x4 = F.relu(self.linear42(x4))
+        x4 = F.relu(self.linear42(x4))'''
 
-        return x1, x2, x3, x4
+        return x1 #, x2, x3, x4
 
     def train_x1(self,dataframe, loss_function, optimizer, epochs):
         log_interval = 5
@@ -101,15 +101,15 @@ class Net(nn.Module):
 
 
 
-  def test_model(self,dataframe, loss_function):
-        log_interval = 1
-        for index, data in enumerate(dataframe):
-            data, target = data[0], data[1]
-            output = self(data)
-            loss = loss_function(output, target)
+    def test_model(self,dataframe, loss_function):
+            log_interval = 1
+            for index, data in enumerate(dataframe):
+                data, target = data[0], data[1]
+                output = self(data)
+                loss = loss_function(output, target)
 
-            if index % log_interval == 0:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(index, index * len(data),
-                                                                               len(data),
-                                                                               100. * index / len(data),
-                                                                               loss.data.item()))
+                if index % log_interval == 0:
+                    print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(index, index * len(data),
+                                                                                   len(data),
+                                                                                   100. * index / len(data),
+                                                                                   loss.data.item()))
